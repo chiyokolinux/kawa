c62568ff21d4f36df0bac7d3b88792ab431b6f49
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-08-11 19:30:26 +0200

    added linked lists, package list sorting, and much more
    things i learned today:
    - this is easier than i thought so far
    - qsort is awesome
    - c++ sucks
    - there are too many open source licenses
    - AAAAAA LINKED LISTS ARE SO CONFUSING ARGHFS WHYYY

--- datatypes.h
+++ datatypes.h
@@ -1,5 +1,5 @@
 enum pkgtype{source, patch, meta, binary};
-enum license{GPLv1, GPLv2, GPLv3, LGPLv1, LGPLv2, LGPLv21, LGPLv3, BSD, FREEOTHER, OTHER, CCBYSA, ZLIB, PUBLICDOMAIN, OSLv3, MIT};
+enum license{GPLv1, GPLv2, GPLv3, LGPLv1, LGPLv2, LGPLv21, LGPLv3, BSD, FREEOTHER, OTHER, CCBYSA, ZLIB, PUBLICDOMAIN, OSLv3, MIT, OPENSSL, PSFL, APACHE, APACHEv2};
 enum scripts{preinstall, preupdate, dobuild, doinstall, postinstall, postupdate};
 
 struct package {
@@ -20,6 +20,12 @@ struct package {
 }
 
 struct pkglist {
-    short pkg_count;
+    int pkg_count;
     struct package packages[];
 } 
+
+struct ll_node {
+    struct package current;
+    struct ll_node *next;
+    struct ll_node *prev;
+}
