4486c7cf22ab1a45260a2453dd18f037c0bfc95d
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-08-11 17:00:39 +0200

    repository syncing works
    i think, this code has never been compiled or run, holy fruck-truck

--- reposync.h
+++ reposync.h
@@ -1,42 +1,56 @@
 #include <stdio.h>
 #include <curl/curl.h>
 
-int sync_repo(char repourl[]
+int sync_repo(char repourl[]);
+int 
 
-int sync_repo(char repourl[]){
+int sync_repo(char reponame[], char repourl[]){
     CURL *curl;
     CURLcode res;
+    int retval = 0;
 
     curl = curl_easy_init();
     if (curl) {
         curl_easy_setopt(curl, CURLOPT_URL, repourl);
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        
+        FILE* indexfile = fopen(strcat(INSTALLPREFIX, strcat("/etc/kawa.d/", strcat(reponame, ".packages.db"))), "w+");
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, indexfile) ;
 
         res = curl_easy_perform(curl);
-        if(res != CURLE_OK)
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        if(res != CURLE_OK) {
+            fprintf(stderr, "Syncing repo %s failed: %s\n", reponame, curl_easy_strerror(res));
+            retval++;
+        }
 
         curl_easy_cleanup(curl);
+        fclose(indexfile);
+    } else {
+        fprintf(stderr, "Syncing repo %s failed: Cannot create cURL object\n", reponame);
+        return ++retval;
     }
-
-    return 0;
+ 
+    return retval;
 }
 
 int sync_all() {
     curl_global_init(CURL_GLOBAL_DEFAULT);
+    int retval = 0;
 
     FILE *fp;
     char reponame[127];
     char repourl[511];
 
-    fp = fopen(INSTALLPREFIX + "/etc/kawa.d/repos.conf", "r");
+    fp = fopen(strcat(INSTALLPREFIX, "/etc/kawa.d/repos.conf"), "r");
 
     while (fscanf(fp, "%s %s", reponame, repourl) != NULL) {
         printf("Syncing Repo %s...", reponame);
-        sync_repo(repourl);
+        retval += sync_repo(reponame, repourl);
     }
     
     fclose(fp);
     
     curl_global_cleanup();
+    
+    return retval;
 }
