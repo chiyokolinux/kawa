ee9aa0e1671cf702c437d2725f0f977247e84624
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-08-12 15:32:45 +0200

    working updates!
    and no working space splitting, for fucks sake WHYYYY

--- update.c
+++ update.c
@@ -3,8 +3,51 @@
 int update() {
     sync_all();
     struct pkglist database = get_all_packages();
-    for (int i = 0; i < database.pkg_count; i++) {
-        struct package currpkg = database.packages[i];
-        
+    struct pkglist installed = get_installed_packages();
+    struct ll_node_update updatepkg;
+    int remote_i = 0;
+    int updatec = 0;
+    for (int i = 0; i < installed.pkg_count; i++) {
+        struct package currpkg_l = installed.packages[i];
+        while (strcmp(currpkg_l.name, database.packages[remote_i].name))
+            remote_i++; // this should work because the package lists are sorted (hopefully)
+        if (strcmp(currpkg_l.version, database.packages[remote_i].version) {
+            struct pkg_update pkgupdt = {
+                .name = currpkg_l.name,
+                .version_local = currpkg_l.version,
+                .version_remote = database.packages[remote_i].version;
+            };
+            updatepkg.current = pkgupdt;
+            updatepkg.next->prev = &updatepkg;
+            updatepkg = *(updatepkg.next);
+            updatec++;
+        }
     }
+    
+    if (!updatec) {
+        printf("No packages need to be updated.\n");
+        return 0;
+    }
+    
+    /* this is a lazy fix, but i'm too tired to do it properly rn */
+    updatepkg = *(updatepkg.prev);
+    updatepkg.next = NULL;
+    while (updatepkg.prev) {
+        updatepkg = *(updatepkg.prev);
+    }
+    
+    printf("The following packages can be updated:\n");
+    
+    while (updatepkg.next != NULL) {
+        printf("%s (%s -> %s) ", updatepkg.current.name, updatepkg.current.version_local, updatepkg.current.version_remote);
+        updatepkg = *(updatepkg.next);
+    }
+    
+    printf("\nDo you wish to install these updates now? [Y/n] ");
+    char response = getchar();
+    if (response == 'n' || response == 'N')
+        return 0;
+    if (response == 'y' || response == 'Y')
+        return upgrade();
+    return 0;
 }
--- update.h
+++ update.h
@@ -2,11 +2,13 @@
 #define UPDATE_H
 
 #include <stdio.h>
+#include <string.h>
 
 #include "reposync.h"
 #include "database.h"
 #include "datatypes.h"
 
 int update();
+int upgrade();
 
 #endif // UPDATE_H
