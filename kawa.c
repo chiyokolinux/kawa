08369b48c499d4c1e53ab197b7de78a94197425e
luk <mail-login+gitlab@protonmail.com>
2020-08-15 14:54:59 +0200

    local changes

--- kawa.c
+++ kawa.c
@@ -6,9 +6,5 @@
 #include "update.h"
 
 int main(int argc, char *argv[]) {
-    if (getuid()) {
-        printf("Because error handling is somewhere on TODO, just run this as root for now and everything should be fine.\n");
-        return -1;
-    }
     return update();
 }
