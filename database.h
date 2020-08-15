f91f931effc7f33bc225b4ec9ef5b6718e5a511c
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-08-15 19:43:16 +0200

    READING ALL DATABASES WORKS
    now onto sorting them

--- database.h
+++ database.h
@@ -10,10 +10,10 @@
 
 struct strarr_retval split_space(char to_split[]);
 void parse_csv_line(char line[], struct package* retval);
-struct pkglist get_packages_from_repo(char reponame[]);
+struct pkglist *get_packages_from_repo(char reponame[]);
 struct pkglist get_all_packages();
 struct pkglist get_installed_packages();
 int compare_strings(const void* a, const void* b);
-struct pkglist sort_package_list(struct pkglist orig_pkglist);
+struct pkglist sort_package_list(struct pkglist *orig_pkglist);
 
 #endif // DATABASE_H
