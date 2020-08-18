f35536c0fed90fb976be7e9decdd7eb41154caf1
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-08-18 18:59:29 +0200

    added dash align

--- show.c
+++ show.c
@@ -6,7 +6,11 @@ int show(char pkgname[]) {
     for (int i = 0; i < database->pkg_count; i++) {
         currpkg = database->packages[i];
         if (!strcmp(currpkg->name, pkgname)) {
-            printf("Information for package %s:\n---------------------------\n", pkgname);
+            printf("Information for package %s:\n-------------------------", pkgname);
+            int dashes_total = strlen(pkgname);
+            for (int dash = 0; dash < dashes_total; dash++)
+                printf("-");
+            printf("\n");
             printf("Name        : %s\n", pkgname);
             printf("Version     : %s\n", currpkg->version);
             printf("Maintainer  : %s\n", currpkg->maintainer);
