c5380fac57154c3f7b6b37cf1c5fd81e21f98dd7
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-08-18 18:57:44 +0200

    added kawa show

--- show.c
+++ show.c
@@ -6,10 +6,16 @@ int show(char pkgname[]) {
     for (int i = 0; i < database->pkg_count; i++) {
         currpkg = database->packages[i];
         if (!strcmp(currpkg->name, pkgname)) {
-            printf("package found\n");
+            printf("Information for package %s:\n---------------------------\n", pkgname);
+            printf("Name        : %s\n", pkgname);
+            printf("Version     : %s\n", currpkg->version);
+            printf("Maintainer  : %s\n", currpkg->maintainer);
+            printf("Type        : %s\n", currpkg->type);
+            printf("License     : %s\n", currpkg->license);
+            printf("Description :\n    %s", currpkg->description);
             return 0;
         }
     }
-    printf("package not found\n");
+    printf("Error: Package %s not found (try kawa sync)\n", pkgname);
     return 1;
 }
