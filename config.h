b6705feed63109ffb7c553c4049d2b16b68dda53
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-08-20 16:54:33 +0200

    vive la couleur!

--- config.h
+++ config.h
@@ -6,4 +6,9 @@
 // Or just simple debugging
 #define INSTALLPREFIX "/home/jonas/kawa_test"
 
+// This is the color in which important stuff (like the occurence in kawa search)
+// is highlighted. Some examples: [0;32m Green; [0;31m Red; [0;33m Yellow; [0;34m Blue
+// Replace the first 0 with a 1 for bold text.
+#define HIGHLIGHTCOLOR "[0;32m"
+
 #endif // CONFIG_H
--- search.c
+++ search.c
@@ -2,14 +2,25 @@
 
 int search(char searchterm[]) {
     struct pkglist *database = get_all_packages();
-    // struct pkglist *installed = get_installed_packages();
-    // int local_i = 0;
+    struct pkglist *installed = get_installed_packages();
     printf("Inst | Name                       | Version        | Type\n");
     printf("-----|----------------------------|----------------|-------\n");
     for (int i = 0; i < database->pkg_count; i++) {
         struct package *currpkg = database->packages[i];
-        if (strstr(currpkg->name, searchterm) != NULL) {
-            printf("%4s | %-26s | %-14s | %s\n", "???", currpkg->name, currpkg->version, currpkg->type);
+        char *pos = strstr(currpkg->name, searchterm);
+        if (pos != NULL) {
+            char package_installed[4] = "no";
+            for (int i2 = 0; i2 < installed->pkg_count; i2++) {
+                if (!strcmp(currpkg->name, installed->packages[i2]->name)) {
+                    strcpy(package_installed, "yes");
+                    break;
+                }
+            }
+            char name_colored[strlen(currpkg->name) + strlen(HIGHLIGHTCOLOR) + 9];
+            int firstpart_len = strlen(currpkg->name) - strlen(pos);
+            // int 
+            sprintf(name_colored, "%.*s\033%s%.*s\033[0m%s", firstpart_len, currpkg->name, HIGHLIGHTCOLOR, strlen(searchterm), pos, pos+strlen(searchterm));
+            printf("%4s | %-37s | %-14s | %s\n", package_installed, name_colored, currpkg->version, currpkg->type);
         }
     }
     return 0;
