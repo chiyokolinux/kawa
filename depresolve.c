ffe0345de71c87e746efc8352dfe3c92ad878ff6
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-08-23 15:24:45 +0200

    added function prototype
    gonna grab a drink now

--- depresolve.c
+++ depresolve.c
@@ -1,2 +1,5 @@
 #include "depresolve.h"
 
+struct dependency_graph *resolve_recursive(struct dependency_graph *nodelist, char *current, struct pkglist *database, struct pkglist *installed) {
+    
+}
--- depresolve.h
+++ depresolve.h
@@ -7,4 +7,6 @@
 #include "database.h"
 #include "datatypes.h"
 
+struct dependency_graph *resolve_recursive(struct dependency_graph *nodelist, char *current, struct pkglist *database, struct pkglist *installed);
+
 #endif // DEPRESOLVE_H
