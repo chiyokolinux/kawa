56c6f07157b55a7ddda70d0258e49c02f7cf1da0
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-09-09 20:51:41 +0200

    added kawa cacheclean stub

---
+++ cacheclean.c
@@ -0,0 +1,17 @@
+#include "cacheclean.h"
+
+int cacheclean() {
+    // init base path
+    char path[strlen(INSTALLPREFIX)+23];
+    strcpy(path, "");
+    strcat(path, INSTALLPREFIX);
+    strcat(path, "/etc/kawa.d/kawafiles/");
+
+    // sprintf path variable into find & delete command
+    char command[strlen(path)+45];
+    sprintf(command, "find %s -name package.tar.\\* -type f -delete", path);
+    
+    // execute find & delete command
+    int retval = system(command);
+    return retval;
+}
---
+++ cacheclean.h
@@ -0,0 +1,10 @@
+#ifndef CACHECLEAN_H
+#define CACHECLEAN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int cacheclean();
+
+#endif // CACHECLEAN_H
