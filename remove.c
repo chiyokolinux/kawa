e716521e69e786380dcd1862049ca9b5cb4a8fba
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-09-11 18:24:11 +0200

    outlined remove.c

---
+++ remove.c
@@ -0,0 +1,20 @@
+#include "remove.h"
+
+int remove(int pkgc, char *pkgnames[]) {
+    for (int i = 2; i < pkgc; i++) {
+        int package_installed = 0;
+        for (int i2 = 0; i2 < installed->pkg_count; i2++) {
+            if (!strcmp(pkgname, installed->packages[i2]->name)) {
+                package_installed = 1;
+                break;
+            }
+        }
+        if (package_installed)
+            kawafile_run(pkgnames[i], "remove");
+        else {
+            printf("Error: package %s is not installed", pkgnames[i]);
+            return 1;
+        }
+    }
+    return 0;
+}
---
+++ remove.h
@@ -0,0 +1,10 @@
+#ifndef REMOVE_H
+#define REMOVE_H
+
+#include <stdio.h>
+
+#include "kawafile.h"
+
+int remove(int pkgc, char *pkgnames[]);
+
+#endif // REMOVE_H
