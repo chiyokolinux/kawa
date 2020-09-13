cd9466715eedfa4a7931669762603e5f9cd3a09d
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-09-13 13:11:06 +0200

    working on removing stuff

--- remove.c
+++ remove.c
@@ -3,6 +3,7 @@
 int pkg_remove(int pkgc, char *pkgnames[]) {
     struct pkglist *installed = get_installed_packages();
     
+    // check if packages *can* be uninstalled
     for (int i = 2; i < pkgc; i++) {
         int package_installed = 0;
         for (int i2 = 0; i2 < installed->pkg_count; i2++) {
@@ -11,12 +12,35 @@ int pkg_remove(int pkgc, char *pkgnames[]) {
                 break;
             }
         }
-        if (package_installed)
-            kawafile_run(pkgnames[i], "remove");
-        else {
+        if (!package_installed) {
             printf("Error: package %s is not installed", pkgnames[i]);
             return 1;
         }
     }
+    
+    printf("The following packages will be installed:\n ");
+    for (int i = 0; i < pkgc; i++) {
+        printf(" %s", pkgnames[i]);
+    }
+    
+    printf("\n\n%d package(s) will be installed, %d package(s) will be removed and %d package(s) will be updated.\n", 0, pkgc, 0);
+    printf("Do you wish to proceed? [Y/n] ");
+    fflush(stdout);
+    char response = getchar();
+    if (response == 'n' || response == 'N')
+        return 0;
+    else if (response == 'y' || response == 'Y' || response == '\n') {
+        int retval = 0;
+        
+        // after updating, install all new packages
+        for (int i = 0; i < pkgc; i++) {
+            retval +=
+        }
+
+        curl_global_cleanup();
+        return retval;
+    } else
+        return 1;
+    
     return 0;
 }
