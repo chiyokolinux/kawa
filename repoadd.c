5037165dbd11116ea4583ac095f128297e2170d9
Emily <36363495+em1lyy@users.noreply.github.com>
2021-03-30 06:09:00 +0200

    holy fucking goddamn shit

--- repoadd.c
+++ repoadd.c
@@ -1,3 +1,21 @@
+/**
+ * this file is part of kawa
+ * Copyright (c) 2020-2021 Emily <elishikawa@jagudev.net>
+ *
+ * kawa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * kawa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with kawa.  If not, see <https://www.gnu.org/licenses/>.
+**/
+
 #include "repoadd.h"
 
 int repoadd(char reponame[], char repourl[]) {
