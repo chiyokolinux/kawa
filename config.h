7fcf5626125348c0351d72a7d08154ccab4f2c1e
Emily <36363495+em1lyy@users.noreply.github.com>
2021-04-14 15:39:00 +0200

    fix

--- config.h
+++ config.h
@@ -24,7 +24,7 @@
 // Prefix for basically everything (also config files!)
 // Use for distributing binary packages of this program (for example)
 // Or just simple debugging
-#define INSTALLPREFIX "/home/jonas/kawa_test"
+#define INSTALLPREFIX "/home/emily/kawa_test"
 
 // This is the color in which important stuff (like the occurence in kawa search)
 // is highlighted. Some examples: [0;32m Green; [0;31m Red; [0;33m Yellow; [0;34m Blue
